9370e95f00259014dcaa4767c95fdbb87410786d
Sindre Vatnaland <69798990+SindreVatnaland@users.noreply.github.com>
2025-01-21 16:12:33 +0100

    Fix crash when stopping packet capture

--- electron/services/packetCapture.ts
+++ electron/services/packetCapture.ts
@@ -21,19 +21,18 @@ export class PacketCapture {
     if (!dolphinSettings) return;
 
     const port = getDolphinPort(dolphinSettings);
-
     console.log("Listening on port", port);
-
-    this.server.on('message', (msg, rinfo) => {
-      this.log.info(`Received message from ${rinfo.address}:${rinfo.port}`);
-      this.log.info(`Message: ${msg}`);
-    });
-
+    this.server.on('message', this.handleMessage);
     this.server.bind(port, "0.0.0.0");
   }
 
   stopPacketCapture(): void {
     this.log.info("Stopping Packet Capture");
-    this.server.close();
+    this.server.off("message", this.handleMessage);
+  }
+
+  private handleMessage(message: Buffer<ArrayBufferLike>, rinfo: dgram.RemoteInfo): void {
+    this.log.info(`Received message from ${rinfo.address}:${rinfo.port}`);
+    this.log.info(`Message: ${message}`);
   }
 }
