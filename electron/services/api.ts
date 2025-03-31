bc182cf8d1d738d9512c1f57314ec4ba9ab4e467
Sindre Vatnaland <69798990+SindreVatnaland@users.noreply.github.com>
2025-03-31 08:23:52 +0200

    Fix slippi api

--- electron/services/api.ts
+++ electron/services/api.ts
@@ -24,30 +24,49 @@ export class Api {
 	}
 
 	async getPlayerRankStats(connectCode: string): Promise<RankedNetplayProfile | undefined> {
-		let response = await axios.post('https://gql-gateway-dot-slippi.uc.r.appspot.com/graphql', {
-			headers: {
-				'Content-Type': 'application/json',
-				Accept: '*/*',
-				'Accept-Encoding': 'gzip, deflate, br',
-				Host: 'gql-gateway-dot-slippi.uc.r.appspot.com',
-				'Accept-Language': 'en-GB,en;q=0.9',
-				Origin: 'https://slippi.gg',
-				'User-Agent':
-					'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
-				Connection: 'keep-alive',
-				Referer: 'https://slippi.gg/',
-				'Content-Length': '838',
-				Priority: 'u=3, i',
-				'apollographql-client-name': 'slippi-web'
+		const response = await axios.post(
+			"https://gql-gateway-dot-slippi.uc.r.appspot.com/graphql",
+			{
+				query: `query AccountManagementPageQuery($cc: String!) {
+          getConnectCode(code: $cc) {
+            user {
+              fbUid
+              displayName
+              connectCode {
+                code
+              }
+              status
+              activeSubscription {
+                level
+                hasGiftSub
+              }
+              rankedNetplayProfile {
+                id
+                ratingOrdinal
+                ratingUpdateCount
+                wins
+                losses
+                dailyGlobalPlacement
+                dailyRegionalPlacement
+                continent
+                characters {
+                  character
+                  gameCount
+                }
+              }
+            }
+          }
+        }`,
+				variables: { cc: connectCode },
 			},
-			operationName: 'AccountManagementPageQuery',
-			variables: {
-				cc: `${connectCode}`,
-				uid: `${connectCode}`
-			},
-			query:
-				'fragment userProfilePage on User {\n  fbUid\n  displayName\n  connectCode {\n    code\n    __typename\n  }\n  status\n  activeSubscription {\n    level\n    hasGiftSub\n    __typename\n  }\n  rankedNetplayProfile {\n    id\n    ratingOrdinal\n    ratingUpdateCount\n    wins\n    losses\n    dailyGlobalPlacement\n    dailyRegionalPlacement\n    continent\n    characters {\n      id\n      character\n      gameCount\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nquery AccountManagementPageQuery($cc: String!, $uid: String!) {\n  getUser(fbUid: $uid) {\n    ...userProfilePage\n    __typename\n  }\n  getConnectCode(code: $cc) {\n    user {\n      ...userProfilePage\n      __typename\n    }\n    __typename\n  }\n}\n'
-		});
+			{
+				headers: {
+					"Content-Type": "application/json",
+					Accept: "*/*",
+					"apollographql-client-name": "slippi-web",
+				},
+			}
+		);
 
 		const data = response?.data.data;
 
