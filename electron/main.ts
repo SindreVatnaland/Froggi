8bf506a7ac7383f419f9452de4a51f78a1f16a94
Sindre Vatnaland <69798990+SindreVatnaland@users.noreply.github.com>
2025-05-12 21:50:36 +0200

    App tray icon

--- electron/main.ts
+++ electron/main.ts
@@ -178,7 +178,7 @@ try {
 	}
 
 	function createTray(): Tray {
-		const imagePath = path.join(__dirname, '../../build/icon.png');
+		const imagePath = path.join(__dirname, '../../build/icon-tray.png');
 		const image = nativeImage.createFromPath(imagePath);
 		tray = new Tray(image.resize({ width: 16, height: 16 }));
 		if (isMac) app.dock.setIcon(image);
